6034b1ffc4d1a854ee4cf2f4fab21e47bfaf1954
firreee <143442624+firreee@users.noreply.github.com>
2024-04-22 02:40:10 -0500

    Create P19.py

---
+++ P19.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Graphic")
+
+canvas = tk.Canvas(root, width=400, height=400, bg="white")
+canvas.pack()
+
+outline_circle = canvas.create_oval(100, 100, 300, 300, outline="light green", width=3)
+
+left_eye = canvas.create_oval(130, 150, 190, 210, fill="red")
+
+right_eye = canvas.create_oval(210, 150, 270, 210, fill="blue")
+
+smile = canvas.create_line(150, 240, 250, 240, fill="yellow", width=15)
+
+triangle = canvas.create_polygon(200, 320, 160, 280, 240, 280, outline="black", fill="white", width=3)
+
+root.mainloop()
